e177ae89e771b99597b7c3d6ee4cb4ff7dc5d7b4
emrysmayell <97519227+emrysmayell@users.noreply.github.com>
2025-07-16 13:50:34 -0700

    Add files via upload

---
+++ php/Syllabus/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en" class="theme-light">
+<head>
+    <?php include$_SERVER['DOCUMENT_ROOT'].'/head.php';?>
+    <title><?php echo $pageTitle ?: 'File'; ?></title>
+    <meta name="description" content="___">
+    <meta name="og:title" property="og:title" content="___">
+    <meta property="og:image" content="___">
+    <meta property="og:url" content="___"/>
+    <link href="___" rel="canonical">
+</head>
+
+<body id="body" class="close">
+
+<div id="header">
+    <?php include$_SERVER['DOCUMENT_ROOT'].'/header.php';?>
+</div>
+
+<div id="content">
+
+
+
+
+<h1>
+    <?php echo $pageTitle ?: 'File'; ?>
+</h1>
+<div class="about-div">
+    <div class="about-text">
+        <p>
+            This is the future location of a Syllabus page.
+        </p>
+    </div>
+</div>
+
+
+
+
+
+</div>
+
+<div id="footer">
+    <?php include$_SERVER['DOCUMENT_ROOT'].'/footer.html';?>
+</div>
+
+</body>
+
+</html>
