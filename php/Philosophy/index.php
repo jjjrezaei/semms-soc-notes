bb290de602c588ff2dad6b3b30f554be7ffa4caf
emrysmayell <97519227+emrysmayell@users.noreply.github.com>
2025-07-16 13:53:10 -0700

    Add files via upload

---
+++ php/Philosophy/index.php
@@ -0,0 +1,6 @@
+<?php
+    $currentDir = __DIR__; // Get current directory
+    $currentPath = basename(__DIR__); //Get current path to directory
+    
+    include$_SERVER['DOCUMENT_ROOT'].'/templates/list.php';
+?>
